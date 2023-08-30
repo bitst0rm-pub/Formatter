8d876c9f8ebc0c226e3a4ab6b216fcc63f6ae49a
bitst0rm <bitst0rm@users.noreply.github.com>
2023-08-30 06:00:39 +0200

    Update

--- src/formatter_htmltidy.py
+++ src/formatter_htmltidy.py
@@ -27,7 +27,6 @@ class HtmltidyFormatter:
 
     def get_cmd(self):
         executable = common.get_intr_exec_path(self.identifier, EXECUTABLES, 'executable')
-
         if not executable:
             return None
 
