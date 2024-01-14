00501b58e9f7f9e1424290026f1aad077c3def1f
bitst0rm <bitst0rm@users.noreply.github.com>
2024-01-14 02:39:52 +0100

    Remove success rate info

--- main.py
+++ main.py
@@ -724,8 +724,8 @@ class RecursiveFormat(common.Base):
         ok = self.CONTEXT['success_count']
         ko = self.CONTEXT['failure_count']
         total = self.CONTEXT['filelist_length']
-        success_rate = '{:.2f}'.format((ok / total) * 100)
-        self.popup_message('Formatting COMPLETED!\n\nOK: %s\nKO: %s\nTotal: %s\nSuccess Rate: %s%%\n\nPlease check the results in:\n%s' % (ok, ko, total, success_rate, self.CONTEXT['cwd']), 'INFO', dialog=True)
+        #success_rate = '{:.2f}'.format((ok / total) * 100)
+        self.popup_message('Formatting COMPLETED!\n\nOK: %s\nKO: %s\nTotal: %s\n\nPlease check the results in:\n%s' % (ok, ko, total, self.CONTEXT['cwd']), 'INFO', dialog=True)
 
     def reset_context(self):
         for key, value in self.CONTEXT.items():
