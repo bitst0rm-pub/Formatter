2d9a2611a5787fd914b480659552b13d88e39131
bitst0rm <bitst0rm@users.noreply.github.com>
2024-01-28 06:35:50 +0100

    Copyright notices

--- modules/formatter_sqlmin.py
+++ modules/formatter_sqlmin.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# @rev          $Format:%H$ ($Format:%h$)
-# @tree         $Format:%T$ ($Format:%t$)
-# @date         $Format:%ci$
-# @author       $Format:%an$ <$Format:%ae$>
 # @copyright    Copyright (c) 2019-present, Duc Ng. (bitst0rm)
 # @link         https://github.com/bitst0rm
 # @license      The MIT License (MIT)
