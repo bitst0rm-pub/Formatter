e8f57aca97c9c0c2ebb68affe0a8f832f4fb16e2
bitst0rm <bitst0rm@users.noreply.github.com>
2024-02-17 00:56:18 +0100

    Add floskell

---
+++ modules/formatter_floskell.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# @copyright    Copyright (c) 2019-present, Duc Ng. (bitst0rm)
+# @link         https://github.com/bitst0rm
+# @license      The MIT License (MIT)
+
+import logging
+from ..core import common
+
+log = logging.getLogger(__name__)
+EXECUTABLES = ['floskell']
+MODULE_CONFIG = {
+    'source': 'https://github.com/ennocramer/floskell',
+    'name': 'Floskell',
+    'uid': 'floskell',
+    'type': 'beautifier',
+    'syntaxes': ['haskell'],
+    'exclude_syntaxes': None,
+    'executable_path': '/path/to/.cabal/bin/floskell',
+    'args': None,
+    'config_path': {
+        'default': 'floskell_rc.json'
+    }
+}
+
+
+class FloskellFormatter(common.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_cmd(self):
+        executable = self.get_executable(runtime_type='haskell')
+        if not executable:
+            return None
+
+        cmd = [executable]
+
+        cmd.extend(self.get_args())
+
+        path = self.get_config_path()
+        if path:
+            cmd.extend(['--config', path])
+
+        cmd.extend(['--'])
+
+        log.debug('Current arguments: %s', cmd)
+        cmd = self.fix_cmd(cmd)
+
+        return cmd
+
+    def format(self):
+        cmd = self.get_cmd()
+        if not self.is_valid_cmd(cmd):
+            return None
+
+        try:
+            exitcode, stdout, stderr = self.exec_cmd(cmd)
+
+            if exitcode > 0:
+                log.error('File not formatted due to an error (exitcode=%d): "%s"', exitcode, stderr)
+            else:
+                return stdout
+        except OSError:
+            log.error('An error occurred while executing the command: %s', ' '.join(cmd))
+
+        return None
