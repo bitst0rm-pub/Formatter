d655269091a230abfcaf038841fdc2e3059b794d
bitst0rm <bitst0rm@users.noreply.github.com>
2024-02-27 12:44:46 +0100

    Add docformatter

---
+++ modules/formatter_docformatter.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# @copyright    Copyright (c) 2019-present, Duc Ng. (bitst0rm)
+# @link         https://github.com/bitst0rm
+# @license      The MIT License (MIT)
+
+import logging
+from ..core import common
+
+log = logging.getLogger(__name__)
+INTERPRETERS = ['python3', 'python']
+EXECUTABLES = ['docformatter']
+MODULE_CONFIG = {
+    'source': 'https://github.com/PyCQA/docformatter',
+    'name': 'Docformatter',
+    'uid': 'docformatter',
+    'type': 'beautifier',
+    'syntaxes': ['python'],
+    'exclude_syntaxes': None,
+    'executable_path': '/path/to/bin/docformatter',
+    'args': None,
+    'config_path': {
+        'default': 'docformatter_rc.toml'
+    },
+    'comment': 'requires "environ": {"PYTHONPATH": ["/lib/python3.7/site-packages"]}'
+}
+
+
+class DocformatterFormatter(common.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_cmd(self):
+        cmd = self.get_combo_cmd(runtime_type='python')
+        if not cmd:
+            return None
+
+        path = self.get_config_path()
+        if path:
+            cmd.extend(['--config', path])
+
+        cmd.extend(['-'])
+
+        log.debug('Current arguments: %s', cmd)
+        cmd = self.fix_cmd(cmd)
+
+        return cmd
+
+    def format(self):
+        cmd = self.get_cmd()
+        if not self.is_valid_cmd(cmd):
+            return None
+
+        try:
+            exitcode, stdout, stderr = self.exec_cmd(cmd)
+
+            if exitcode > 0:
+                self.print_exiterr(exitcode, stderr)
+            else:
+                return stdout
+        except OSError:
+            self.print_oserr(cmd)
+
+        return None
