ff3b3be2aaf0b74bcc07ecce438877f23625784a
bitst0rm <bitst0rm@users.noreply.github.com>
2024-05-15 14:17:22 +0200

    Add biome

---
+++ modules/formatter_biome.py
@@ -0,0 +1,66 @@
+import logging
+from ..core import common
+
+log = logging.getLogger(__name__)
+INTERPRETERS = ['node']
+EXECUTABLES = ['biome']
+MODULE_CONFIG = {
+    'source': 'https://github.com/biomejs/biome',
+    'name': 'Biome',
+    'uid': 'biome',
+    'type': 'beautifier',
+    'syntaxes': ['js', 'jsx', 'ts', 'tsx', 'json'],
+    'exclude_syntaxes': None,
+    'executable_path': '/path/to/bin/biome (standalone binary) or /path/to/node_modules/.bin/biome',
+    'args': None,
+    'config_path': {
+        'default': 'biome_rc.json'
+    },
+    'comment': 'interpreter_path can be set to use node, otherwise omid it to use standalone binary.'
+}
+
+
+class BiomeFormatter(common.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_cmd(self):
+        cmd = self.get_combo_cmd(runtime_type='node')
+        if not cmd:
+            return None
+
+        cmd.extend(['format', '--colors', 'off'])
+
+        cmd.extend(self.get_args())
+
+        path = self.get_config_path()
+        if path:
+            cmd.extend(['--config-path', path])
+
+        file = self.get_pathinfo()['path']
+        dummy = file if file else 'dummy.' + self.get_assigned_syntax()
+        cmd.extend(['--stdin-file-path', dummy])
+
+        cmd.extend(['-'])
+
+        log.debug('Current arguments: %s', cmd)
+        cmd = self.fix_cmd(cmd)
+
+        return cmd
+
+    def format(self):
+        cmd = self.get_cmd()
+        if not self.is_valid_cmd(cmd):
+            return None
+
+        try:
+            exitcode, stdout, stderr = self.exec_cmd(cmd)
+
+            if exitcode > 0:
+                self.print_exiterr(exitcode, stderr)
+            else:
+                return stdout
+        except OSError:
+            self.print_oserr(cmd)
+
+        return None
