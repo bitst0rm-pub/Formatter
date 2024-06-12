24a85b570d12d3067482c866cc5b6187eb7869bf
bitst0rm <bitst0rm@users.noreply.github.com>
2024-06-12 14:49:14 +0200

    fix(prettier): Remove redundant DOTFILES constant

--- modules/formatter_prettierd.py
+++ modules/formatter_prettierd.py
@@ -5,7 +5,6 @@ log = logging.getLogger(__name__)
 INTERPRETERS = ['node']
 EXECUTABLES = ['prettierd']
 DOTFILES = ['.prettierrc', '.prettierrc.json', '.prettierrc.yml', '.prettierrc.yaml', '.prettierrc.json5', '.prettierrc.js', 'prettier.config.js', '.prettierrc.mjs', 'prettier.config.mjs', '.prettierrc.cjs', 'prettier.config.cjs', '.prettierrc.toml']
-DOTFILES = []
 MODULE_CONFIG = {
     'source': 'https://github.com/fsouza/prettierd',
     'name': 'Prettierd',
