3c0a08c1a897ec7cb81d41291538e481c6d4a0f3
bitst0rm <bitst0rm@users.noreply.github.com>
2024-09-07 22:30:36 +0200

    fix: Resolve cursor jumping issue on save caused by `'ensure_newline_at_eof_on_save'` ST setting

--- plugin/replace_view_content_command.py
+++ plugin/replace_view_content_command.py
@@ -4,4 +4,13 @@ import sublime_plugin
 
 class ReplaceViewContentCommand(sublime_plugin.TextCommand):
     def run(self, edit, result, region):
-        self.view.replace(edit, sublime.Region(region[0], region[1]), result)
+        target_region = sublime.Region(region[0], region[1])
+        size = sublime.Region(region[0], self.view.size())
+
+        if target_region == size and self.view.settings().get('ensure_newline_at_eof_on_save', False):
+            line_endings = self.view.settings().get('default_line_ending', 'system')
+            # Windows uses \r\n (2 chars) while Unix-based systems use \n (1 char)
+            line_ending_length = 2 if line_endings == 'windows' or (line_endings == 'system' and sublime.platform() == 'windows') else 1
+            target_region = sublime.Region(region[0], region[1] - line_ending_length)
+
+        self.view.replace(edit, target_region, result)
