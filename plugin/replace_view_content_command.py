8e93e247059a87e9cd3b8c0a841c976f89a1b75b
bitst0rm <bitst0rm@users.noreply.github.com>
2024-09-13 11:00:54 +0200

    bump: 1.6.5

--- plugin/replace_view_content_command.py
+++ plugin/replace_view_content_command.py
@@ -9,7 +9,7 @@ class ReplaceViewContentCommand(sublime_plugin.TextCommand):
 
         if target_region == size and self.view.settings().get('ensure_newline_at_eof_on_save', False):
             line_endings = self.view.settings().get('default_line_ending', 'system')
-            # Windows uses \r\n (2 chars) while Unix-based systems use \n (1 char)
+            # Windows \r\n (2 chars), Unix \n (1 char)
             line_ending_length = 2 if line_endings == 'windows' or (line_endings == 'system' and sublime.platform() == 'windows') else 1
             target_region = sublime.Region(region[0], region[1] - line_ending_length)
 
