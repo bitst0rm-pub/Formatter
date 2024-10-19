bbd38b9e45126e258ff9ed22e6e341d53c3a25ef
bitst0rm <bitst0rm@users.noreply.github.com>
2024-10-19 02:57:47 +0200

    feat: Add `yamlmax` (build-in) to supported plugins

---
+++ modules/formatter_yamlmax.py
@@ -0,0 +1,45 @@
+import sublime
+
+from ..core import Module, log
+from ..libs import yaml
+
+DOTFILES = []
+MODULE_CONFIG = {
+    'source': 'build-in',
+    'name': 'YAML',
+    'uid': 'yamlmax',
+    'type': 'beautifier',
+    'syntaxes': ['yaml'],
+    'exclude_syntaxes': None,
+    'executable_path': None,
+    'args': None,
+    'config_path': {
+        'default': 'yamlmax_rc.json'
+    },
+    'comment': 'Build-in, no "executable_path".'
+}
+
+
+class YamlmaxFormatter(Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def format(self):
+        path = self.get_config_path()
+        if path:
+            with open(path, 'r', encoding='utf-8') as file:
+                data = file.read()
+
+            cmd = sublime.decode_value(data)
+            log.debug('Command: %s', cmd)
+
+        try:
+            text = self.get_text_from_region(self.region)
+            obj = yaml.safe_load(text)
+            result = yaml.dump(obj, **cmd)
+
+            return result
+        except ValueError as e:
+            log.status('File not formatted due to ValueError: "%s"', e)
+
+        return None
