a939f77471988a952228faf58e37c90be374ca7f
bitst0rm <bitst0rm@users.noreply.github.com>
2024-10-21 11:48:32 +0200

    feat: Add `yj` as converter to the supported plugins for `yjxtoyaml`, `yjxtojson`, `yjxtotoml`, `yqxtohcl`

--- README.md
+++ README.md
@@ -159,6 +159,7 @@ _Need more? see:_ [Configuration](#configuration) and [Development](#development
 | Converter | Input | Output | Req. | Config |
 | ------ | :------: | :------: | :------: | :------: |
 | [yq](https://github.com/mikefarah/yq) | YAML, JSON, CSV, TSV, XML, TOML, LUA, TEXT | YAML, JSON, PROPS, CSV, TSV, XML, BASE64, URI, TOML, SHELL, LUA | None | -- |
+| [yj](https://github.com/sclevine/yj) | YAML, JSON, TOML, HCL | YAML, JSON, TOML, HCL | None | -- |
 
 > [!TIP]
 >
---
+++ modules/formatter_yjxtohcl.py
@@ -0,0 +1,56 @@
+from ..core import Module
+
+EXECUTABLES = ['yj']
+DOTFILES = []
+MODULE_CONFIG = {
+    'source': 'https://github.com/sclevine/yj',
+    'name': 'Yj X->HCL',
+    'uid': 'yjxtohcl',
+    'type': 'converter',
+    'syntaxes': ['yaml', 'json', 'toml', 'hcl'],
+    'exclude_syntaxes': None,
+    'executable_path': '/path/to/bin/yj',
+    'args': None,
+    'config_path': None,
+    'comment': 'No "config_path", use "args" instead.'
+}
+
+
+class YjxtohclFormatter(Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_cmd(self):
+        executable = self.get_executable(runtime_type=None)
+        if not executable:
+            return None
+
+        cmd = [executable]
+
+        cmd.extend(self.get_args())
+
+        syntax = self.get_assigned_syntax()
+        if syntax in ['yaml', 'json', 'toml', 'hcl']:
+            char = syntax[1] if syntax == 'hcl' else syntax[0]
+            cmd.extend(['-' + char + 'c', '-'])
+        else:
+            cmd = None
+
+        return cmd
+
+    def format(self):
+        cmd = self.get_cmd()
+        if not cmd:
+            return None
+
+        try:
+            exitcode, stdout, stderr = self.exec_cmd(cmd)
+
+            if exitcode > 0:
+                self.print_exiterr(exitcode, stderr)
+            else:
+                return stdout
+        except Exception as e:
+            self.print_oserr(cmd, e)
+
+        return None
